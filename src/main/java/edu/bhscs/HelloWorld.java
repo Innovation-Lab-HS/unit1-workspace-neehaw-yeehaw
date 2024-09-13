7b8315a4c52f8ef09edcd9e7029181dd5df77d21
neehaw-yeehaw <1117230@apps.nsd.org>
2024-09-13 18:19:54 +0000

    raghhh

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+  // comments are entered after two forward slashes
+  // all code in java sits inside a class
+  // every java program needs a public main method
+  public static void main(String[] args) {
+    // for the next while, ALL the code you write will live inside main()
+    System.out.println("Hello. ^-^");
+    System.out.println("");
+    System.out.println("WEEEEEEEEEEEEE");
+    // semicolons identify the end of a line of code
+  }
+}
